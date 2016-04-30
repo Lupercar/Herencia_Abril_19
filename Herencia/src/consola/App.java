73c65c69caebc6fc3894f0260b3f1e808ad7509e
JLGG <hotolete@gmail.com>
2016-05-01 00:21:46 +0200

    Casting o moldeo en clase App entre un objeto p de la clase Producto y otro pc de la clase hija ProductoCaducable

--- Herencia/src/consola/App.java
+++ Herencia/src/consola/App.java
@@ -2,12 +2,17 @@ package consola;
 
 import java.util.Date;
 
+import entidades.Producto;
 import entidades.ProductoCaducable;
 
 public class App {
 
 	public static void main(String[] args) {
 		ProductoCaducable pc = new ProductoCaducable(1, "Producto1 Caducable", 1.00, new Date() );
-		System.out.println(pc); //utiliza el toString de la clase que hereda Producto, muestra todo menos la fecha.
+		System.out.println("pc\n"+pc); //utiliza el toString de la clase que hereda Producto, muestra todo menos la fecha.
+		
+		//casting o moldeo
+		Producto p = pc; 
+		System.out.println("p\n"+p);
 	}
 }
