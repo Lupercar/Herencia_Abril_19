0d86cc38cf2fa180961fa082c8b13e78936d924d
JLGG <hotolete@gmail.com>
2016-05-01 00:07:07 +0200

    Muestro el toString del objeto PC que es de la clase ProductoCaducable, pero como ProductoCaducable no ha reescrito el método toString de la clase Padre Producto, se muestra el toString de Producto.

--- Herencia/src/consola/App.java
+++ Herencia/src/consola/App.java
@@ -8,5 +8,6 @@ public class App {
 
 	public static void main(String[] args) {
 		ProductoCaducable pc = new ProductoCaducable(1, "Producto1 Caducable", 1.00, new Date() );
+		System.out.println(pc); //utiliza el toString de la clase que hereda Producto, muestra todo menos la fecha.
 	}
 }
