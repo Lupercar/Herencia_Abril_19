5fc3bc045cab8d93e58998cc2b3c10d679e57759
luperca <hotolete@gmail.com>
2016-05-01 11:40:18 +0200

    Creación clase ProductoNoCaducable, le meto 2 constructores uno sin parámetros que llama al constructor con parámetros de la clase y otro con parámetros que llama al constructor de la clase Padre

--- Herencia/src/entidades/ProductoNoCaducable.java
+++ Herencia/src/entidades/ProductoNoCaducable.java
@@ -1,5 +1,15 @@
 package entidades;
 
 public class ProductoNoCaducable extends Producto{
+	
+//	Constructor sin parámetros que llama al constructor con parámetros ProductoCaducable
+	public ProductoNoCaducable() {
+		this(0, "", 0.0); //centralizo todo en el constructor con parámetros
+	}
 
+//	Constructor que llama al constructor de la clase Padre
+	public ProductoNoCaducable(int codigo, String nombre, double precio) {
+		super(codigo, nombre, precio); 
+	}
+	
 }
