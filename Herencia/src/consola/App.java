5492a7c4ee50737407d58a2ffc71d0427199a9d2
luperca <hotolete@gmail.com>
2016-05-01 11:46:31 +0200

    Creo objeto ProductoNoCaducable en clase App y reescribo método toString en clase ProductoNoCaducable

--- Herencia/src/consola/App.java
+++ Herencia/src/consola/App.java
@@ -4,12 +4,16 @@ import java.util.Date;
 
 import entidades.Producto;
 import entidades.ProductoCaducable;
+import entidades.ProductoNoCaducable;
 
 public class App {
 
 	public static void main(String[] args) {
 		ProductoCaducable pc = new ProductoCaducable(1, "Producto1 Caducable", 1.00, new Date() );
+		ProductoNoCaducable pnc = new ProductoNoCaducable(2, "Producto2 No Caducable", 1.00);
+		
 		System.out.println("pc\n"+pc); //utiliza el toString de la clase que hereda Producto, muestra todo menos la fecha.
+		System.out.println("pnc\n"+pnc);
 		
 		//casting o moldeo
 		Producto p = pc; 
--- Herencia/src/entidades/ProductoNoCaducable.java
+++ Herencia/src/entidades/ProductoNoCaducable.java
@@ -1,15 +1,23 @@
 package entidades;
 
-public class ProductoNoCaducable extends Producto{
-	
-//	Constructor sin parámetros que llama al constructor con parámetros ProductoCaducable
+public class ProductoNoCaducable extends Producto {
+
+	// Constructor sin parámetros que llama al constructor con parámetros
+	// ProductoCaducable
 	public ProductoNoCaducable() {
-		this(0, "", 0.0); //centralizo todo en el constructor con parámetros
+		this(0, "", 0.0); // centralizo todo en el constructor con parámetros
 	}
 
-//	Constructor que llama al constructor de la clase Padre
+	// Constructor que llama al constructor de la clase Padre
 	public ProductoNoCaducable(int codigo, String nombre, double precio) {
-		super(codigo, nombre, precio); 
+		super(codigo, nombre, precio);
 	}
-	
+
+	@Override
+	public String toString() {
+		return "ProductoNoCaducable [" +
+				super.toString()+
+				"]";
+	}
+
 }
